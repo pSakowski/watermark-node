8bc0fcd593fe4bc3895da684f0d17aa7ead9e8aa
Piotr Sakowski <psakowski1213@gmail.com>
2023-02-11 01:54:44 +0100

    add try...catch

--- App.js
+++ App.js
@@ -3,33 +3,41 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 const addTextWatermarkToImage = async function(inputFile, outputFile, text) {
-  const image = await Jimp.read(inputFile);
-  const font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
-  const textWidth = Jimp.measureText(font, text);
-  const x = (image.bitmap.width - textWidth) / 2;
-  const y = (image.bitmap.height - 32) / 2;
-  image.print(font, x, y, text);
-  await image.quality(100).writeAsync(outputFile);
-  console.log(`Watermarked image has been successfully created: ${outputFile}`);
-  startApp();
+  try {
+    const image = await Jimp.read(inputFile);
+    const font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
+    const textWidth = Jimp.measureText(font, text);
+    const x = (image.bitmap.width - textWidth) / 2;
+    const y = (image.bitmap.height - 32) / 2;
+    image.print(font, x, y, text);
+    await image.quality(100).writeAsync(outputFile);
+    console.log(`File ${outputFile} was created successfully.`);
+  } catch (error) {
+    console.log('Something went wrong... Try again!');
+    startApp();
+  }
 };
 
 const addImageWatermarkToImage = async function(inputFile, outputFile, watermarkFile) {
-  const image = await Jimp.read(inputFile);
-  const watermark = await Jimp.read(watermarkFile);
-  const x = (image.bitmap.width - watermark.bitmap.width) / 2;
-  const y = (image.bitmap.height - watermark.bitmap.height) / 2;
-  image.composite(watermark, x, y, {
-    mode: Jimp.BLEND_SOURCE_OVER,
-    opacitySource: 0.7
-  });
-  await image.quality(100).writeAsync(outputFile);
-  console.log(`Watermarked image has been successfully created: ${outputFile}`);
-  startApp();
+  try {
+    const image = await Jimp.read(inputFile);
+    const watermark = await Jimp.read(watermarkFile);
+    const x = (image.bitmap.width - watermark.bitmap.width) / 2;
+    const y = (image.bitmap.height - watermark.bitmap.height) / 2;
+    image.composite(watermark, x, y, {
+      mode: Jimp.BLEND_SOURCE_OVER,
+      opacitySource: 0.7
+    });
+    await image.quality(100).writeAsync(outputFile);
+    console.log(`File ${outputFile} was created successfully.`);
+  } catch (error) {
+    console.log('Something went wrong... Try again!');
+    startApp();
+  }
 };
 
 const prepareOutputFilename = (filename) => {
-  const [ name, ext ] = filename.split('.');
+  const [name, ext] = filename.split('.');
   return `${name}-with-watermark.${ext}`;
 };
 
